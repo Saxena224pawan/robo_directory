9289100fced1d6c548dfb4e45e530001336e2db8
Saxena224pawan <saxenapawan224@gmail.com>
2020-05-17 01:13:02 +0530

    Updates

---
+++ precache-manifest.96815101d358ba99df11410198147a94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7521d18973876d595bdc65ef1ddb73d",
+    "url": "/robo_directory/index.html"
+  },
+  {
+    "revision": "2080a8c0c2429c624f13",
+    "url": "/robo_directory/static/css/main.f2457d6b.chunk.css"
+  },
+  {
+    "revision": "e87cfcf9af9d0a9333fd",
+    "url": "/robo_directory/static/js/2.b508ab51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo_directory/static/js/2.b508ab51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2080a8c0c2429c624f13",
+    "url": "/robo_directory/static/js/main.fcaf3644.chunk.js"
+  },
+  {
+    "revision": "863c3454743df9cc4c38",
+    "url": "/robo_directory/static/js/runtime-main.17108ba8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robo_directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
